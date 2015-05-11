61db44951a327bb2a3ec1998b629ec36f52a9b8f
silversurfer <mathermark@hotmail.com>
2015-05-11 21:41:43 +0100

    Added tips label to meetings#index

--- config/application.rb
+++ config/application.rb
@@ -55,8 +55,8 @@ module RocketHorse
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile = ['*.js', '*.css']
-    config.assets.initialize_on_precompile = true
+    #config.assets.precompile = ['*.js', '*.css']
+    #config.assets.initialize_on_precompile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
